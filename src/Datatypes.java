b2d5f6fe403ed4fddcedf07e6a0ea736de969689
Your Name <youremail@example.com>
2025-09-28 10:55:37 +0530

    datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int age=19;
+        char symbol='@';
+        String name="RAVI";
+        double height=22.4;
+        float price=3.4f;
+        System.out.println("Hi my age is "+age);
+        System.out.print(symbol);
+        System.out.println(name);
+        System.out.println("The height is "+height);
+        System.out.println("The Price is "+price);
+
+    }
+}
