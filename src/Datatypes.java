e369eb627390ea751f60953900bf5b53aacb2dfe
Your Name <youremail@example.com>
2025-09-28 11:00:51 +0530

    datatypes updates

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -5,11 +5,18 @@ public class Datatypes {
         String name="RAVI";
         double height=22.4;
         float price=3.4f;
+        boolean forSale=true;
         System.out.println("Hi my age is "+age);
         System.out.print(symbol);
         System.out.println(name);
         System.out.println("The height is "+height);
         System.out.println("The Price is "+price);
+        if(forSale){
+            System.out.println("THIS IS FOR SALE");
+        }
+        else{
+            System.out.println("This is not for sale");
+        }
 
     }
 }
