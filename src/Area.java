065f94b131e4413c2535c44fbfd0009ee197e098
Your Name <youremail@example.com>
2025-09-28 12:34:58 +0530

    Area of Rectangle updates

--- src/Area.java
+++ src/Area.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Enter your Length:");
         int Length = scanner.nextInt();
         System.out.println("Your Length of the Rectangle is " + Length);
-
+        System.out.print("Enter your Width:");
         int Width = scanner.nextInt();
         System.out.println("Your width of the rectangle is " + Width);
 
