4595b655ea01bd9b03582d4d751efee9b934a5fa
Your Name <youremail@example.com>
2025-10-01 22:52:34 +0530

    operator

---
+++ src/Arith.java
@@ -0,0 +1,16 @@
+public class Arith {
+    public static void main(String[] args) {
+        // Arithmetic Operator
+        int x=10;
+        int y=2;
+        int z=4;
+        x-=y;
+        z++;
+        z++;
+        z++;
+        y--;
+        y--;
+        System.out.println(z);
+        System.out.println(y);
+    }
+}
