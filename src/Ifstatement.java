c8e7f42b73cece57c7a3da8ea9ffd852f0fa52ac
Your Name <youremail@example.com>
2025-10-02 18:56:20 +0530

    if

---
+++ src/Ifstatement.java
@@ -0,0 +1,14 @@
+public class Ifstatement {
+    public static void main(String[] args) {
+        int age=-8;
+
+        if(age > 18){
+            System.out.println("You are an adult");
+        } else if (age<0) {
+            System.out.println("you are not yet born");
+
+        } else{
+            System.out.println("you are an child");
+        }
+    }
+}
