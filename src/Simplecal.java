7c16bb93075771e0f453175bfef086fa447de77a
Your Name <youremail@example.com>
2025-10-03 23:37:10 +0530

    calculator

---
+++ src/Simplecal.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Simplecal {
+    public static void main(String[] args) {
+        Scanner cal=new Scanner(System.in);
+        int number1;
+        int number2;
+        char op;
+        double result;
+        System.out.println("Enter a number1");
+        number1= cal.nextInt();
+        System.out.println("Enter a number2");
+        number2= cal.nextInt();
+        System.out.println("Enter the operator:");
+        op=cal.next().charAt(0);
+        if (op=='+'){
+            result=number1+number2;
+            System.out.println("The addition of the two number is "+result);
+        }
+        else if (op=='-'){
+            result=number1-number2;
+            System.out.println("The subtraction of the two number is "+result);
+        }
+        else if (op=='*'){
+            result=number1*number2;
+            System.out.println("The multiplication of the two number is "+result);
+        }
+        else if (op=='/'){
+            if(number2!=0){
+                result=number1/number2;
+                System.out.println("The division of the two number is "+result);
+            }
+            else {
+                System.out.println("It is not divided by 0");
+            }
+        }
+
+    }
+}
