478b3a0f24eac242c96cf641f1f2c5e8ab4638e5
Your Name <youremail@example.com>
2025-10-04 19:02:10 +0530

    name

--- src/Ifstatement.java
+++ src/Ifstatement.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Ifstatement {
     public static void main(String[] args) {
+        Scanner vayasu = new Scanner(System.in);
         int age=-8;
-
-        if(age > 18){
+        String name;
+        System.out.println("Enter your name");
+name= vayasu.nextLine();
+if(name.isEmpty()){
+    System.out.println("you didnt enter your name:");
+}
+else{
+    System.out.println("Hello"+name+"!");
+}
+       if(age > 18){
             System.out.println("You are an adult");
         } else if (age<0) {
             System.out.println("you are not yet born");
@@ -10,5 +21,6 @@ public class Ifstatement {
         } else{
             System.out.println("you are an child");
         }
+       vayasu.close();
     }
 }
