2057c2efd0b40d287d9a69a1e79f5ae396e2fc42
Your Name <youremail@example.com>
2025-10-05 19:59:14 +0530

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,13 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name="devdesign.ravi";
+        int length=name.length();
+        char letter=name.charAt(0);
+        int index=name.indexOf('s');
+        int last=name.lastIndexOf('e');
+        System.out.println(length);
+        System.out.println(letter);
+        System.out.println(index);
+        System.out.println(last);
+    }
+}
