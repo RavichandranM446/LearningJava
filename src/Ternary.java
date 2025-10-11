12daa0d6521dfbce82bc86f9118398214bb20bf0
Your Name <youremail@example.com>
2025-10-11 22:16:14 +0530

    Ternaryoperator

---
+++ src/Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int score=75;
+        String passorfail=(score >= 60) ? "pass":"Fail";
+        System.out.println(passorfail);
+    }
+}
