da11827ee81112ed2fa78d5676267e4454623271
Your Name <youremail@example.com>
2025-10-22 21:38:31 +0530

    While Number

--- src/Whileproblem.java
+++ src/Whileproblem.java
@@ -7,6 +7,6 @@ public class Whileproblem {
             System.out.println("Enter a number between 1-10:");
             number= pro.nextInt();
         }
-        System.out.println("You picked 70" + number);
+        System.out.println("You picked " + number);
     }
 }
